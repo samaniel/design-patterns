6cd879f09ffe804bed7069fa801a0597aa1fcef7
samaniel <44196411+samaniel@users.noreply.github.com>
2024-09-09 18:05:32 +0200

    Update lazy.java

--- creational/singleton/lazy.java
+++ creational/singleton/lazy.java
@@ -1,1 +1,24 @@
 
+public class SingletonLazy {
+
+    // Instancia única de la clase, inicialmente nula
+    private static SingletonLazy instancia;
+
+    // Constructor privado para evitar instanciación desde fuera
+    private SingletonLazy() {
+        // Inicialización de recursos si es necesario
+    }
+
+    // Método público para obtener la instancia única de la clase
+    public static SingletonLazy getInstance() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+
+    // Método de ejemplo en la clase Singleton
+    public void mostrarMensaje() {
+        System.out.println("Hola desde Singleton con Lazy Initialization!");
+    }
+}
