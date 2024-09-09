45a17731eb63048dcea5b38f8c2438f7735efc4e
samaniel <44196411+samaniel@users.noreply.github.com>
2024-09-09 18:07:55 +0200

    Create main.java

---
+++ creational/singleton/springboot/main.java
@@ -0,0 +1,20 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private SingletonService singletonService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        singletonService.mostrarMensaje();
+    }
+}
