4f042c84fd95c67a1176801f81e770cd0c50f289
samaniel <44196411+samaniel@users.noreply.github.com>
2024-09-09 18:06:10 +0200

    Update eager.java

--- creational/singleton/eager.java
+++ creational/singleton/eager.java
@@ -1,1 +1,20 @@
+public class SingletonEager {
 
+    // Instancia única de la clase, creada al cargar la clase
+    private static final SingletonEager instancia = new SingletonEager();
+
+    // Constructor privado para evitar instanciación desde fuera
+    private SingletonEager() {
+        // Inicialización de recursos si es necesario
+    }
+
+    // Método público para obtener la instancia única de la clase
+    public static SingletonEager getInstance() {
+        return instancia;
+    }
+
+    // Método de ejemplo en la clase Singleton
+    public void mostrarMensaje() {
+        System.out.println("Hola desde Singleton con Eager Initialization!");
+    }
+}
