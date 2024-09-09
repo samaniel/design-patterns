eb42832d7fe292f75198af9d907e62b30154fbe2
samaniel <44196411+samaniel@users.noreply.github.com>
2024-09-09 18:07:22 +0200

    Update Service.java

--- creational/singleton/springboot/Service.java
+++ creational/singleton/springboot/Service.java
@@ -1,1 +1,8 @@
+@Service
+public class SingletonService {
 
+    // Método de ejemplo en el bean Singleton
+    public void mostrarMensaje() {
+        System.out.println("Hola desde Singleton en Spring Boot!");
+    }
+}
